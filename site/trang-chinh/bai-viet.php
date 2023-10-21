95e1e716744fa543f9e199824b6839fdeab798ee
Hienpt <phamthehienapple@gmail.com>
2023-10-21 19:13:56 +0700

    Initial commit

---
+++ site/trang-chinh/bai-viet.php
@@ -0,0 +1,42 @@
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-lg-12">
+            <!-- Post content-->
+            <article>
+                <!-- Post header-->
+                <header class="mb-4">
+                    <!-- Post title-->
+                    <h1 class="fw-bolder mb-1">Hienpt Shop
+                    </h1>
+                    <!-- Post meta content-->
+                    
+                </header>
+                <!-- Preview image figure-->
+                <figure class="mb-4"><img class="img-fluid rounded" src="../../content/images/banners/abi.jpg"
+                        alt="..." />
+                </figure>
+                <!-- Post content-->
+                <section class="mb-5">
+                    <p class="fs-5 mb-4">Hienpt Shop - Nơi tạo nên phong cách đường phố của bạn.
+                    </p>
+                    <p class="fs-5 mb-4">Hienpt Shop là một trang web bán quần áo trực tuyến phục vụ khách hàng với
+                        những mẫu thiết kế độc đáo, đầy phong cách và mang đậm tính chất đường phố. Với sự sáng tạo và
+                        đam mê với nền văn hóa đường phố, Hienpt Shop đã tạo ra một không gian mua sắm trực tuyến đặc
+                        biệt, nơi bạn có thể tìm kiếm những sản phẩm mang tính cá nhân và thể hiện phong cách riêng của
+                        mình.
+
+                    </p>
+                    <h2 class="fw-bolder mb-4 mt-5">Đặc điểm</h2>
+                    <p class="fs-5 mb-4">Điểm đặc biệt của Hienpt Shop nằm ở việc mang đến cho khách hàng những thiết kế
+                        quần áo độc đáo và đa dạng. Với sự phát triển và thịnh hành của nền văn hóa đường phố, Hienpt
+                        Shop cung cấp một bộ sưu tập đa dạng với các mẫu áo, quần, áo khoác và phụ kiện đường phố phù
+                        hợp với xu hướng và phong cách hiện đại. Bạn có thể thể hiện cá tính của mình và tạo nên phong
+                        cách độc đáo thông qua những sản phẩm tại đây.</p>
+
+                </section>
+            </article>
+
+
+        </div>
+    </div>
+</div>
---
+++ site/trang-chinh/gioi-thieu.php
@@ -0,0 +1,54 @@
+<div class="wrapper" id="wrapper">
+    <div class="" id="banner">
+        <div class="parallax text-center" style="background-image:url(../../content/images/banners/Aboutus.webp);">
+            <div class="parallax-pattern-overlay">
+                <div class="container text-center text-white" style="height:650px;padding-top:15%;">
+                    <h2 class="display-2 shadow font-weight-bold">Hienpt Shop</h2>
+                    <h3 class="learn shadow font-weight-bold">Về chúng tôi</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="aboutus" id="aboutus">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="heading text-center">
+                        <h2 class="text-danger">Hienpt Shop</h2>
+                            <img src="../../uploaded/abi.jpg" alt="">
+                        <h4>“Hienpt Shop được sáng lập bởi Phạm Thế Hiển, những người con của Sài Gòn với
+                            niềm đam mê bất tận và mãnh liệt với nền văn hóa đường phố. Cả hai lớn lên trong những bức
+                            hình graffiti đầy nghệ thuật và những trận battle breakdance nảy lửa, với Hip hop chảy trong
+                            máu của họ như một phần không thể thiếu.
+
+                            Phạm Thế Hiển, với niềm đam mê mãnh liệt về nền văn hóa đường phố, đã phát triển khả năng mỹ
+                            thuật của mình. Bên cạnh việc hiểu rõ về hiphop và văn hoá đại chúng, anh đã sử dụng tài
+                            năng của mình để tạo ra Hienpt Shop - một trang web bán quần áo mang trong mình tinh thần và
+                            phong cách của văn hoá đường phố.
+
+                            Hienpt Shop không chỉ là nơi để khách hàng mua sắm quần áo, mà còn là một không gian thể
+                            hiện sự yêu mến và sâu sắc với nền văn hóa đường phố. Trang web này mang đến những sản phẩm
+                            đậm chất hip hop, với các thiết kế độc đáo và phong cách nghệ thuật đường phố. Với sự gắn
+                            kết và tận tụy đến từ Phạm Thế Hiển, Hienpt Shop hy vọng sẽ truyền cảm hứng và lan tỏa
+                            nền văn hóa đường phố đến với mọi người..</h4>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.8410320579424!2d105.74095901476346!3d21.039045785992847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454962c0b6523%3A0x5c76c67564d9d1b9!2zUGjhu5EgVHLhu4tuaCBWxINuIELDtCwgUGjGsMahbmcgQ2FuaCwgSMOgIE7hu5lpLCBWaWV0bmFt!5e0!3m2!1sen!2s!4v1635607330848!5m2!1sen!2s"
+                        width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+
+</div>
